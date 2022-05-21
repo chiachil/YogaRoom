3d6b8a5f3c22bdf6df0ea9cd1b752ecf53652dfd
chiachil <65595327+chiachil@users.noreply.github.com>
2022-05-21 17:08:59 +0800

    First version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./style.css";
-// import ClassSetting from "./pages/ClassSetting";
-// import ClassPage from "./pages/ClassPage";
 import ClassSetting from "./pages/ClassSetting";
 
 export const App = () => {
