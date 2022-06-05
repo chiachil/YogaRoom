41de25f8428475c18eb19f0145bcd4b0b4414be6
chiachil <65595327+chiachil@users.noreply.github.com>
2022-06-06 02:19:55 +0800

    fix: change image path to production

--- src/global/path.js
+++ src/global/path.js
@@ -1,2 +1,2 @@
-export const smImageUrl = "http://localhost:3000/images/poses-sm/";
-export const lgImageUrl = "http://localhost:3000/images/poses-lg/";
+export const smImageUrl = "https://yogaroom-24504.firebaseapp.com/images/poses-sm/";
+export const lgImageUrl = "https://yogaroom-24504.firebaseapp.com/images/poses-lg/";
