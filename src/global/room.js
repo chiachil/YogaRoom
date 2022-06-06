1900dc9429510d07b4d5f5b7b45187adf77f359b
chiachil <65595327+chiachil@users.noreply.github.com>
2022-06-06 16:08:02 +0800

    refactor: adjust image path and put room data in global variable

--- src/global/room.js
+++ src/global/room.js
@@ -1,2 +1,2 @@
-export const colorArr = ["#d7b0a9", "#81a59a", "#77a0a9", "#81797a"];
+export const colorArr = ["#81797a", "#d7b0a9", "#81a59a", "#77a0a9"];
 export const voiceArr = ["English", "中文"];
