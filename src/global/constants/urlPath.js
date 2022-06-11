34b9ea60ac41868bd2a45b325e33b1b866c8b96c
chiachil <65595327+chiachil@users.noreply.github.com>
2022-06-11 12:15:51 +0800

    fix: change image url from local to https

--- src/global/constants/urlPath.js
+++ src/global/constants/urlPath.js
@@ -4,7 +4,7 @@ export const lgImageUrl =
   "https://yogaroom-24504.firebaseapp.com/images/poses-lg/";
 export const roomImage =
   "https://yogaroom-24504.firebaseapp.com/images/room/1.jpg";
-export const demo1Image = "http://localhost:3000/images/demo/demo1.gif";
-export const demo2Image = "http://localhost:3000/images/demo/demo2.gif";
-export const demo3Image = "http://localhost:3000/images/demo/demo3.gif";
-export const domain = "http://localhost:3000/";
+export const demo1Image = "https://yogaroom-24504.firebaseapp.com/images/demo/demo1.gif";
+export const demo2Image = "https://yogaroom-24504.firebaseapp.com/images/demo/demo2.gif";
+export const demo3Image = "https://yogaroom-24504.firebaseapp.com/images/demo/demo3.gif";
+export const domain = "https://yogaroom-24504.firebaseapp.com/";
