044966d66c96415276415791cd445d3ab023fe2d
chiachil <65595327+chiachil@users.noreply.github.com>
2022-06-13 07:32:59 +0800

    doc: add images for welcome page

--- src/global/constants/urlPath.js
+++ src/global/constants/urlPath.js
@@ -4,7 +4,9 @@ export const lgImageUrl =
   "https://yogaroom-24504.firebaseapp.com/images/poses-lg/";
 export const roomImage =
   "https://yogaroom-24504.firebaseapp.com/images/room/1.jpg";
-export const demo1Image = "https://yogaroom-24504.firebaseapp.com/images/demo/demo1.gif";
-export const demo2Image = "https://yogaroom-24504.firebaseapp.com/images/demo/demo2.gif";
-export const demo3Image = "https://yogaroom-24504.firebaseapp.com/images/demo/demo3.gif";
+export const demo1Image =
+  "https://yogaroom-24504.firebaseapp.com/images/demo/demo1.gif";
+export const demo2Image = "https://yogaroom-24504.firebaseapp.com/images/images/demo/demo2.gif";
+export const demo3Image = "https://yogaroom-24504.firebaseapp.com/images/images/demo/demo3.gif";
+export const demo4Image = "https://yogaroom-24504.firebaseapp.com/images/images/demo/demo4.png";
 export const domain = "https://yogaroom-24504.firebaseapp.com/";
