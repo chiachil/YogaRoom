124610e717c44d508b76e0016efc6619996460b6
chiachil <65595327+chiachil@users.noreply.github.com>
2022-06-13 07:34:56 +0800

    refactor: manage login state by context hook

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SetFlow from "./pages/SetFlow";
 import GoPractice from "./pages/GoPractice";
 import HomePage from "./pages/HomePage";
+import FavList from "./pages/FavList";
+import { useState } from "react";
+import { LoginContext } from "./context/userContext";
 
 export const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/goPractice" element={<GoPractice />} />
-        <Route path="/setFlow" element={<SetFlow />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <LoginContext.Provider
+      value={{
+        loggedIn,
+        setLoggedIn,
+      }}
+    >
+      <Router>
+        <Routes>
+          <Route path="/goPractice" element={<GoPractice />} />
+          <Route path="/setFlow" element={<SetFlow />} />
+          <Route path="/favList" element={<FavList />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </LoginContext.Provider>
   );
 };
