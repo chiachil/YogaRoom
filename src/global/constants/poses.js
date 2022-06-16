23cc0394cfab9e57c5da8ecd75898809ae4f893a
chiachil <65595327+chiachil@users.noreply.github.com>
2022-06-17 05:08:53 +0800

    doc: add pose images

--- src/global/constants/poses.js
+++ src/global/constants/poses.js
@@ -1,13 +1,38 @@
 export const poses = [
   {
+    engName: "Arm Pressure",
+    chiName: "腳交叉雙臂支撐式",
+    imagePath: "ArmPressure.svg",
+  },
+  {
+    engName: "Balancing Stick",
+    chiName: "平衡木式",
+    imagePath: "BalancingStick.svg",
+  },
+  {
+    engName: "Balancing Table",
+    chiName: "平衡桌面式",
+    imagePath: "BalancingTable.svg",
+  },
+  {
     engName: "Boat",
     chiName: "船式",
     imagePath: "Boat.svg",
   },
   {
+    engName: "Bow",
+    chiName: "弓式",
+    imagePath: "Bow.svg",
+  },
+  {
     engName: "Bridge",
     chiName: "橋式",
     imagePath: "Bridge.svg",
+  },
+  {
+    engName: "Butterﬂy",
+    chiName: "束角式",
+    imagePath: "Butterﬂy.svg",
   },
   {
     engName: "Cat",
@@ -45,6 +70,16 @@ export const poses = [
     imagePath: "CrescentMoon.svg",
   },
   {
+    engName: "Crow",
+    chiName: "烏鴉式",
+    imagePath: "Crow.svg",
+  },
+  {
+    engName: "Dancer",
+    chiName: "舞王式",
+    imagePath: "Dancer.svg",
+  },
+  {
     engName: "Downward Facing Dog",
     chiName: "下犬式",
     imagePath: "DownwardFacingDog.svg",
@@ -53,6 +88,16 @@ export const poses = [
     engName: "Easy Pose",
     chiName: "簡易坐式",
     imagePath: "EasyPose.svg",
+  },
+  {
+    engName: "Extended Side Angle",
+    chiName: "側角式",
+    imagePath: "ExtendedSideAngle.svg",
+  },
+  {
+    engName: "Fireﬂy",
+    chiName: "螢火蟲式",
+    imagePath: "Fireﬂy.svg",
   },
   {
     engName: "Fish",
@@ -65,14 +110,55 @@ export const poses = [
     imagePath: "Chaturanga.svg",
   },
   {
+    engName: "Garland",
+    chiName: "深蹲合掌式",
+    imagePath: "Garland.svg",
+  },
+
+  {
+    engName: "Goddess",
+    chiName: "女神式",
+    imagePath: "Goddess.svg",
+  },
+  {
+    engName: "Half Bow",
+    chiName: "跪立半弓式",
+    imagePath: "HalfBow.svg",
+  },
+  {
+    engName: "Half Moon",
+    chiName: "半月式",
+    imagePath: "Halfmoon.svg",
+  },
+  {
+    engName: "Handstand",
+    chiName: "手肘倒立式",
+    imagePath: "Handstand.svg",
+  },
+  {
     engName: "Happy Baby",
     chiName: "快樂嬰兒式",
     imagePath: "HappyBaby.svg",
   },
   {
+    engName: "Headstand",
+    chiName: "頭倒立式",
+    imagePath: "HeadStand.svg",
+  },
+  {
+    engName: "Hero",
+    chiName: "英雄坐式",
+    imagePath: "Hero.svg",
+  },
+  {
     engName: "Lord of the Fishes",
     chiName: "半魚王式",
     imagePath: "LordoftheFishes.svg",
+  },
+  {
+    engName: "Lotus",
+    chiName: "蓮花坐式",
+    imagePath: "Lotus.svg",
   },
   {
     engName: "Lunge",
@@ -85,9 +171,24 @@ export const poses = [
     imagePath: "Mountain.svg",
   },
   {
+    engName: "Pigeon",
+    chiName: "鴿子式",
+    imagePath: "Pigeon.svg",
+  },
+  {
     engName: "Plank",
     chiName: "平板式",
     imagePath: "Plank.svg",
+  },
+  {
+    engName: "Plow",
+    chiName: "犁式",
+    imagePath: "Plow.svg",
+  },
+  {
+    engName: "Scale",
+    chiName: "蓮花支撐式",
+    imagePath: "Scale.svg",
   },
   {
     engName: "Seated Forward Bend",
@@ -100,6 +201,16 @@ export const poses = [
     imagePath: "SeatedTwist.svg",
   },
   {
+    engName: "Side Lunge",
+    chiName: "側角伸展式",
+    imagePath: "SideLunge.svg",
+  },
+  {
+    engName: "Side Plank",
+    chiName: "側板式",
+    imagePath: "SidePlank.svg",
+  },
+  {
     engName: "Standing Forward Fold",
     chiName: "站立前屈式",
     imagePath: "StandingForwardFold.svg",
@@ -110,9 +221,29 @@ export const poses = [
     imagePath: "SupineSpinalTwist.svg",
   },
   {
+    engName: "Three Legged Dog",
+    chiName: "單腳下犬式",
+    imagePath: "ThreeLeggedDog.svg",
+  },
+  {
+    engName: "Tree",
+    chiName: "樹式",
+    imagePath: "Tree.svg",
+  },
+  {
+    engName: "Triangle",
+    chiName: "三角式",
+    imagePath: "Triangle.svg",
+  },
+  {
     engName: "Upward Facing Dog",
     chiName: "上犬式",
     imagePath: "UpwardFacingDog.svg",
+  },
+  {
+    engName: "Upward Bow",
+    chiName: "輪式",
+    imagePath: "UpwardBow.svg",
   },
   {
     engName: "Warrior One",
